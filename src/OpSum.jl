3282fb5550b5af0ca3ba9d45d2c5d08d3c3f09e2
Lukas Devos <ldevos98@gmail.com>
2025-02-04 14:10:54 -0500

    Add DAWG dictionary

--- src/OpSum.jl
+++ src/OpSum.jl
@@ -2,10 +2,14 @@ module OpSum
 
 export Trie
 export SDAWG
+export DAWGDictionary
 
 using Dictionaries
 
+# Data structures
+# ---------------
 include("trie.jl")
 include("dawg.jl")
+include("dawgdict.jl")
 
 end
---
+++ src/dawgdict.jl
@@ -0,0 +1,36 @@
+struct DAWGDictionary{I,T,C} <: AbstractDictionary{I,T}
+    indices::SDAWGIndices{C,I}
+    values::Vector{T}
+end
+
+Base.keys(dict::DAWGDictionary) = getfield(dict, :indices)
+_values(dict::DAWGDictionary) = getfield(dict, :values)
+
+# Constructors
+# ------------
+
+function DAWGDictionary(inds, values)
+    I = sortperm(inds)
+    indices_dawg = SDAWGIndices(inds[I])
+    return DAWGDictionary(indices_dawg, values[I])
+end
+
+depth(dict::DAWGDictionary) = depth(keys(dict))
+state_register(dict::DAWGDictionary, d) = state_register(keys(dict), d)
+
+# Tokens
+# ------
+Dictionaries.istokenizable(dict::DAWGDictionary) = true
+
+Dictionaries.tokenized(dict::DAWGDictionary) = _values(dict)
+
+function Dictionaries.istokenassigned(dict::DAWGDictionary, (_slot, index))
+    return isassigned(_values(dict), index)
+end
+function Dictionaries.istokenassigned(dict::DAWGDictionary, index::Int)
+    return isassigned(_values(dict), index)
+end
+
+@inline Dictionaries.gettokenvalue(dict::DAWGDictionary, (_slot, index)) =
+    _values(dict)[index]
+@inline Dictionaries.gettokenvalue(dict::DAWGDictionary, index::Int) = _values(dict)[index]

4e1247f91d9c2f219e41f2e4eb4c7425f953799d
Lukas Devos <ldevos98@gmail.com>
2025-03-05 15:57:33 -0500

    random testcode

--- test/basics/test_basics.jl
+++ test/basics/test_basics.jl
@@ -1,8 +1,14 @@
 using OpSum
 using Test: @test, @testset
-using OpSum: depth, opsum_state_machine, compress_state_machine
+using OpSum:
+    depth,
+    opsum_state_machine,
+    compress_state_machine,
+    opsum_vertex_operators,
+    opsum_bond_coefficients
 
-N = 6
+N = 8
+T = Int
 
 using OpSum.PauliOperators: B, I, X, Y, Z, E
 
@@ -12,11 +18,15 @@ end
 ops_twosite = map(1:(N - 1)) do i
     return vcat(fill(B, i - 1), [X, X], fill(E, N - i - 1))
 end
+ops_bookkeeping = map(0:N) do i
+    return vcat(fill(B, i), fill(E, N - i))
+end
 
-list = vcat(ops_onesite, ops_twosite)
+list = vcat(ops_onesite, ops_twosite, ops_bookkeeping)
 dawg = DAWGDictionary(sort!(list), collect(1:length(list)))
 
-Ws, = opsum_state_machine(dawg)
+Ws = opsum_vertex_operators(dawg)
+Ms = opsum_bond_coefficients(dawg)
 # Ws = compress_state_machine(Ws)
 @info "simple"
 foreach(display, Ws)
@@ -25,13 +35,19 @@ foreach(display, Ws)
 ops_nnn = map(1:(N - 2)) do i
     return vcat(fill(B, i - 1), [X, I, X], fill(E, N - i - 2))
 end
-list = sort(vcat(ops_onesite, ops_twosite, ops_nnn, [fill(B, N), fill(E, N)]))
-dawg = DAWGDictionary(list, trues(length(list)))
+
+ops_bookkeeping = map(0:N) do i
+    return vcat(fill(B, i), fill(E, N - i))
+end
+
+list = vcat(ops_onesite, ops_twosite, ops_nnn, ops_bookkeeping)
+coeffs = ones(Int, length(list))
+coeffs[end .- (1:length(ops_bookkeeping)) .+ 1] .= 0
+dawg = DAWGDictionary(list, coeffs)
 
 Ws, Ms = opsum_state_machine(dawg);
 
-ops_all_to_all = Vector{OpSum.PauliOperators.PauliBasis}[fill(B, N), fill(E, N)]
-
+ops_all_to_all = Vector{OpSum.PauliOperators.PauliBasis}[ops_bookkeeping...]
 for i in 1:N, j in (i + 1):N, k in (j + 1):N, l in (k + 1):N
     ops = fill(I, N)
     ops[1:(i - 1)] .= Ref(B)
@@ -44,8 +60,13 @@ for i in 1:N, j in (i + 1):N, k in (j + 1):N, l in (k + 1):N
 end
 
 dawg = DAWGDictionary(
-    ops_all_to_all, vcat([0.0], ones(Float64, length(ops_all_to_all)), [0.0])
+    ops_all_to_all,
+    vcat(
+        zeros(T, length(ops_bookkeeping)),
+        ones(T, length(ops_all_to_all) - length(ops_bookkeeping)),
+    ),
 )
+
 Ws, Ms = opsum_state_machine(dawg)
 @info "uncompressed"
 io = IOContext(stdout, :limit => false)

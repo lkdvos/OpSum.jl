23d14aa6ffc1f45ce3c4feaf1084ee319989ee44
Lukas Devos <ldevos98@gmail.com>
2026-02-09 16:22:44 -0500

    remove obsolete definition

--- src/utility/linalg.jl
+++ src/utility/linalg.jl
@@ -1,7 +1,3 @@
 function MatrixAlgebraKit.copy_input(::typeof(svd_compact), A::SparseMatrixDOK)
     return MatrixAlgebraKit.copy_input(svd_compact, Matrix(A))
 end
-
-function SparseArraysBase.default_mul!!(C, A, B, α::Number = true, β::Number = false)
-    return β * C + α * A * B
-end
